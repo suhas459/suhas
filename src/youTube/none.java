7aff5c2cdce08fd27452f47cad70bd69a13d86ae
suhas <suhaspowar2856@gmail.com>
2023-02-02 07:40:17 +0530

    new none is created

---
+++ src/youTube/none.java
@@ -0,0 +1,11 @@
+package youTube;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class none {
+  @Test (timeOut = 5)
+  public void f() {
+	  Reporter.log("hii", true);
+  }
+}
